afd029e4d5f262bf6eca2b0a28578eb743fbc948
Clint Veasey <6726598+veasey@users.noreply.github.com>
2024-05-22 07:50:01 +0100

    Update dalek.py
    genuinely better
    - random scroll events make screenshots look better
    - keyboard input
    - does not rely on resolution specific images

--- dalek.py
+++ dalek.py
@@ -1,31 +1,80 @@
 import time
 import pyautogui
+from random import randrange
 
-timesClicked = 0
+pyautogui.FAILSAFE = False
 
-buttonsToFind = [
-    'button.png'
+typage = [
+    '(',
+    ']',
+    '{',
+    'a',
+    'print_r',
+    'cast',
+    's',
+    ';',
+    'RepositoryInterface',
+    'Class',
+    'Interface',
+    'Trait',
+    'Abstract',
+    'Exception',
+    'RuntimeException',
+    'there is no way a bee should be able to fly.',
+    'its wings are too small to get its fat little body off the ground.',
+    'The bee, of course, flies anyway',
+    'because bees don’t care what humans think is impossible.',
+    'Yellow, black. Yellow, black. Yellow, black. Yellow, black.',
+    'Ooh, black and yellow!',
+    'Let’s shake it up a little.',
+    'Barry! Breakfast is ready!',
+    'Ooming!',
+    'Hang on a second.',
+    'Hello?',
+    'Barry?',
+    'Adam?',
+    'Can you believe this is happening?',
+    'I can’t. I’ll pick you up.',
+    'Looking sharp.',
+    'Use the stairs. Your father paid good money for those.',
+    'Sorry. I’m excited.'
 ]
 
 while 1:
 
-    for button in buttonsToFind:
-        
-        buttonCoordinates = False
-        print('searching for ' + str(button));
+    startx, starty = pyautogui.position()
 
-        try:
-            buttonCoordinates = pyautogui.locateOnScreen(button)
-        except:
-            pass
+    time.sleep(randrange(15) + 90)
 
-        if (buttonCoordinates):
-            
-            x, y= pyautogui.locateCenterOnScreen(button)
-            pyautogui.moveTo(x, y, duration = 0.1)
+    checkx, checky = pyautogui.position()
+
+    if startx == checkx:
+        if starty == checky:
+
+            # scroll
+            toPage = randrange(2)
+            if toPage == 1:
+                pyautogui.press('pageup')
+            if toPage == 2:
+                pyautogui.press('pagedown')
+
+            # click
+            pyautogui.moveTo(56, 2125)
             pyautogui.leftClick()
 
-            timesClicked = timesClicked + 1
-            print('Clicking ' + str(button) + ' (' +  str(timesClicked) + ' clicks)')
+            # return pos
+            pyautogui.moveTo(startx, starty)
 
-    time.sleep(3)
+
+            checkx, checky = pyautogui.position()
+
+            if startx == checkx:
+                if starty == checky:
+
+                    # type
+                    if (toPage > 2):
+                        interval = ((randrange(9)+1) / 10)
+                        pyautogui.typewrite(typage[randrange(len(typage))], interval)
+
+
+
